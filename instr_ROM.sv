1016c35e5c242b8d852644cda91aae1f0d646999
tamarasagakova <78051761+tamarasagakova@users.noreply.github.com>
2023-03-14 10:38:34 -0700

    Update instr_ROM.sv

--- instr_ROM.sv
+++ instr_ROM.sv
@@ -5,7 +5,7 @@ module instr_ROM (
   input       [9:0] prog_ctr,    // prog_ctr	  address pointer
   output logic[ 8:0] mach_code);
 
-  logic[9:0] core[2**9];
+  logic[9:0] core[2**10];
   initial							    // load the program
     $readmemb("mach_code.txt",core);
 
