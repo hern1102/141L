716c2ab55c58519b789afdb19d7310cec920df7f
tamarasagakova <78051761+tamarasagakova@users.noreply.github.com>
2023-03-14 09:49:55 -0700

    updates bits from 9 to 10

--- BRANCH_LUT.sv
+++ BRANCH_LUT.sv
@@ -5,13 +5,4 @@ module BRANCH_LUT (
   logic[9:0] core[2**8];
   assign branch_addr = core[index];
 
-
-  /*always_comb case(branch_addr)
-	0: branch_addr = 00001111;   // go back 5 spaces
-	1: branch_addr = 00001110;   // go ahead 20 spaces
-	2: branch_addr = 00001100;  // go back 1 space   1111_1111_1111
-	default: branch_addr = 'b0;  // hold PC 
-
-  endcase*/
-
 endmodule
