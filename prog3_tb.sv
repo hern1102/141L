aae230015332ff694c2cce2294a13b38aab3b313
tamarasagakova <78051761+tamarasagakova@users.noreply.github.com>
2023-03-22 18:09:35 -0700

    Update prog3_tb.sv

--- prog3_tb.sv
+++ prog3_tb.sv
@@ -11,7 +11,7 @@ logic[  7:0] cto,		       // how many bytes hold the pattern? (32 max)
              cts,		       // how many patterns in the whole string? (253 max)
 		     ctb;		       // how many patterns fit inside any byte? (160 max)
 logic        ctp;		       // flags occurrence of patern in a given byte
-logic[  4:0] pat;              // pattern to search for
+logic[  7:0] pat;              // pattern to search for
 logic[255:0] str2; 	           // message string
 logic[  7:0] mat_str[32];      // message string parsed into bytes
 
@@ -54,48 +54,52 @@ initial begin
   //initialize jump table
     DUT.jump1.core[0] = 10'b0000101101; //45
     DUT.jump1.core[1] = 10'b0001010010; //82
-    DUT.jump1.core[2] = 10'b0010100011; //163
-    DUT.jump1.core[3] = 10'b0010111001; //185
-    DUT.jump1.core[4] = 10'b0011001111; //207
+    DUT.jump1.core[2] = 10'b0010100101; //165
+    DUT.jump1.core[3] = 10'b0011000001; //193
+    DUT.jump1.core[4] = 10'b0011011101; //221
     DUT.jump1.core[5] = 10'b0001111010; //122
-    DUT.jump1.core[6] = 10'b0011100101; //229
+    DUT.jump1.core[6] = 10'b0011111001; //249
 
   //initialize brunch table
-    DUT.branch1.core[0] = 10'b0100101110; //302
+    DUT.branch1.core[0] = 10'b0101000010; //322
     DUT.branch1.core[1] = 10'b0000001100; //12
     DUT.branch1.core[2] = 10'b0001001100; //76
     DUT.branch1.core[3] = 10'b0001101001; //105
     DUT.branch1.core[4] = 10'b0001111010; //122
-    DUT.branch1.core[5] = 10'b0011110110; //246
-    DUT.branch1.core[6] = 10'b0100000100; //260
-    DUT.branch1.core[7] = 10'b0100010010; //274
-    DUT.branch1.core[8] = 10'b0100100000; //288
+    DUT.branch1.core[5] = 10'b0100001010; //266
+    DUT.branch1.core[6] = 10'b0100011000; //280
+    DUT.branch1.core[7] = 10'b0100100110; //294
+    DUT.branch1.core[8] = 10'b0100110100; //308
 
 // program 3
 // pattern we are looking for; experiment w/ various values
-  pat = {5'b0000,3'b000};//{5'b10101,3'b000};//{$random,3'b000};
+  //pat = {5'b00000,3'b000};//{5'b10101,3'b000};//{$random,3'b000};
+  pat = {5'b11111,3'b000};
+  //pat = {5'b10101,3'b000};
   str2 = 0;
   DUT.dm1.core[32] = pat;
   for(int i=0; i<32; i++) begin
 // search field; experiment w/ various vales
-    mat_str[i] = 8'b00000000;//8'b01010101;// $random;
+    //mat_str[i] = 8'b00000000;//8'b01010101;// $random;
+    mat_str[i] = 8'b11111111;
+    //mat_str[i] = 8'b01010101;
 	DUT.dm1.core[i] = mat_str[i];   
 	str2 = (str2<<8)+mat_str[i];
   end
   ctb = 0;
   for(int j=0; j<32; j++) begin
-    if(pat==mat_str[j][4:0]) ctb++;
-    if(pat==mat_str[j][5:1]) ctb++;
-    if(pat==mat_str[j][6:2]) ctb++;
-    if(pat==mat_str[j][7:3]) ctb++;
+    if(pat[7:3]==mat_str[j][4:0]) ctb++;
+    if(pat[7:3]==mat_str[j][5:1]) ctb++;
+    if(pat[7:3]==mat_str[j][6:2]) ctb++;
+    if(pat[7:3]==mat_str[j][7:3]) ctb++;
   end
   cto = 0;
   for(int j=0; j<32; j++) 
-    if((pat==mat_str[j][4:0]) | (pat==mat_str[j][5:1]) |
-       (pat==mat_str[j][6:2]) | (pat==mat_str[j][7:3])) cto ++;
+    if((pat[7:3]==mat_str[j][4:0]) | (pat[7:3]==mat_str[j][5:1]) |
+       (pat[7:3]==mat_str[j][6:2]) | (pat[7:3]==mat_str[j][7:3])) cto ++;
   cts = 0;
   for(int j=0; j<252; j++) begin
-    if(pat==str2[255:251]) cts++;
+    if(pat[7:3]==str2[255:251]) cts++;
 	str2 = str2<<1;
   end        	    
   #10ns reset   = 1'b1;      // pulse request to DUT
